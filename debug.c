7792dad1c2cf5b4dd6f2367c8ab9ad18318b8e20
del9ra <107867458+del9ra@users.noreply.github.com>
2023-04-10 18:44:50 -0400

    Create debug.c

---
+++ debug.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+
+int main(void)
+{
+    // Ask for your name and where live
+    name = get_string("What is your name? ")
+    location = get_string("Where do you live? ")
+
+    // Say hello
+    print("Hello, %i, from %i!", name, location)
+}

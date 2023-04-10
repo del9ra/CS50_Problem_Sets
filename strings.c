5ad5571d98f9fb93b14d0019480f02602baeb002
del9ra <107867458+del9ra@users.noreply.github.com>
2023-04-10 19:05:06 -0400

    Create strings.c

---
+++ strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("Input: ");
+    printf("Output: ");
+    int length = strlen(s);
+    for(int i = 0, n = strlen(s); i<n; i++)
+    {
+        printf("%c", s[i]);
+    }
+    printf("\n");
